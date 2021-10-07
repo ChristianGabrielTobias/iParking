66f9baf3b06b70786fde20b5e37aa58b499ef8ac
marianacazzoto <MARIANA.CAZZOTO@bandtec.com.br>
2021-10-07 13:42:36 -0300

    Criação da tabela funciomário (Augusto)

--- Script sprint2.sql
+++ Script sprint2.sql
@@ -15,6 +15,16 @@ insert into Funcionário ( nomefuncionário, cargo, cpf, telefonefixo, celular )
 ('Murilo Pinto', 'Estagiário', 65647871231, 1145663433, 11934587766),
 ('Maicon Douglas', 'Líder', 77838342471, 1133798654, 11997965435),
 ('Brenda Rosa', 'Monitora', 36345479888, 1165564489, 11909066784),
-('Márcia Teixeira', 'Estagiária', 71245699233, 1123234871, 11914234567);
+('Márcia Teixeira', 'Estagiária', 71245699233, 1123234871, 11914234567),
+('Gabriel Fernandez', 'Estagiário', 73345625253, 1123234567, 11989766347),
+('Fernanda Barbosa', 'Líder', 66245625563, 1175734871, 11972334547),
+('Gabriel Silva', 'Líder', 67745625665, 1122734231, 11922366547),
+('Natan', 'Gerente', 11745525345, 1125534791, 11951367587),
+('Genésio', 'Gerente', 67745625665, 1122734231, 11922366547),
+('Anastácia', 'Monitora', 99745235775, 1131534771, 11945366549),
+('Junior', 'Estagiário', 91275235345, 1195234851, 11959266219),
+('Beatriz', 'Estagiária', 95385238995, 1132224741, 11942786579),
+('Jeniffer', 'Estagiária', 11745145788, 1135637781, 11988321569),
+('Alisson', 'Estagiário', 44742232695, 1131884274, 11943876316);
 
 select * from funcionário;
