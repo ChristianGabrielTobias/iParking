808c35278f1ba9c5ee1e6fcd4d13a3815994b7b2
AlanComidal <alan.comidal@bandtec.com.br>
2021-10-11 21:44:03 -0300

    Criando tabela de leitura e inserindo dados

--- Banco de dados/Script sprint2.sql
+++ Banco de dados/Script sprint2.sql
@@ -44,8 +44,8 @@ create table empresa(
 insert into empresa (nomeEmpresa, email, cnpj, telefone, data_criaçao, senha) values
 ('EasyParking', 'easy@estacionamento.com.br', '00001234567899', '99999-9999', '2021-09-10', 'easy123'),
 ('Park Plus', 'parkplus@estacionamento.com.br', '00009876543211', '12345-5432', '2021-10-01', 'plus0000'),
-('HaVagas?', 'havagas@estacionamento.com.br', '00006547893215', '65415-4125', '2021-06-31', 'vagas999'),
-('vagas ja', 'vagasja@estacionamento.com.br', '000032165474163', '45612-0176', '2021-07-28', '1234vagas');
+('HaVagas?', 'havagas@estacionamento.com.br', '00006547893215', '65415-4125', '2021-06-30', 'vagas999'),
+('vagas ja', 'vagasja@estacionamento.com.br', '00003216547416', '45612-0176', '2021-07-28', '1234vagas');
 
 select * from empresa;
 
@@ -68,3 +68,25 @@ insert into estacionamento (nome,rua,numero,bairro,cep,qtdVagas) values
 ('vagas ja','Chauncey Valleys','365','Park dos Buritis','20521110','69');
 
 select * from estacionamento;
+
+-- Alan (Criação tabela Leitura)
+create table Leitura(
+idLeitura int primary key auto_increment,
+fkSensor int,
+horarioEntrada datetime,
+horarioSaida datetime,
+placa varchar(45)
+)auto_increment = 1;
+
+insert into Leitura (horarioEntrada,horarioSaida,placa) values
+('2021-09-02 12:02:34','2021-09-02 12:30:41','hrf-2354'),
+('2021-09-02 14:13:20','2021-09-02 15:32:11','roi-1371'),
+('2021-09-04 08:45:31','2021-09-04 14:21:04','sur1o10'),
+('2021-09-04 08:50:21','2021-09-04 09:12:36','tyt7c71'),
+('2021-09-04 10:12:02','2021-09-04 10:20:19','pon-5871'),
+('2021-09-04 02:02:02','2021-09-04 20:20:20','vin-2020'),
+('2021-09-07 03:23:00','2021-09-07 05:54:08','you-7031'),
+('2021-09-08 00:00:35','2021-09-09 13:46:38','ban3d73'),
+('2021-09-11 18:47:22','2021-09-11 19:45:34','sur1o10');
+
+select * from Leitura;
