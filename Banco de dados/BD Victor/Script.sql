9a6c9a13f7161e66afc0c53ac8ccef9d72977a38
victor veniti <victorveniti@hotmail.com>
2021-10-13 11:21:04 -0300

    node

---
+++ Banco de dados/BD Victor/Script.sql
@@ -0,0 +1,13 @@
+create database Iparking;
+use Iparking;
+
+create table funcionario(
+idFuncionario int primary key,
+nome varchar(45),
+cargo varchar(45),
+cpf char(11),
+telefone varchar(10),
+celular char(11)
+);
+
+
