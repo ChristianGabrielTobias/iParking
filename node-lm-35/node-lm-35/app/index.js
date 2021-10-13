58905d1c8e0660e7f1bc052d1566039b50537fe7
lozanodev <madakey@live.com>
2021-10-13 14:45:16 -0300

    node do Alan

--- node-lm-35/node-lm-35/app/index.js
+++ node-lm-35/node-lm-35/app/index.js
@@ -44,4 +44,3 @@ dht11 = sensors.dht11({minHum:50, maxHum:100, minTemp: 18, maxTemp: 23})
 
 console.log('DHT11 - Umidade: ' + dht11[0])
 console.log('DHT11 - Temperatura: ' + dht11[1].toFixed(2))
-
