eb322dde06a5bd2eb923a2a9d13215c50860abf1
victor veniti <victorveniti@hotmail.com>
2021-12-01 20:56:31 -0300

    pro will

---
+++ Pro will/api-projeto-site/src/routes/usuarios.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+var veiculoController = require("../controllers/veiculoController")
+
+router.get("/", function(req, res) {
+    usuarioController.testar(req, res);
+});
+
+router.get("/listar", function(req, res) {
+    usuarioController.listar(req, res);
+});
+
+router.post("/cadastrar", function(req, res) {  
+    usuarioController.cadastrar(req, res);
+})
+
+// router.get("/cadastrar/usuarioName", function(req, res) {
+//     usuarioController.nomeUsuario(req, res)
+// }) /*Pegando o nome do usuario*/
+
+router.post("/veic/:idUsuario", function(req, res) {
+    veiculoController.cadastrarveiculo(req, res)
+})/*Criando a rota de cadastrar veiculo*/
+
+router.get("/veic/metrica", function(req, res) {
+    veiculoController.veiculosPorMarca(req, res)
+})
+
+router.post("/autenticar", function(req, res) {
+    usuarioController.entrar(req, res);
+});
+  
+module.exports = router;
